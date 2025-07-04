6a649f259ef91063cec24faa2a590ea01a79dbcd
WizCodeDesign <182226633+WizCodeDesign@users.noreply.github.com>
2025-07-04 15:28:24 +0300

    Added keyboard support for numbers and operators.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # odin-calculator  
 <br/><br/>
-This is an odin Project where I build a calculator that can add, subtract, divide and multiply numbers that the user inputs from prompt.
+This is an odin Project where I build a calculator that can add, subtract, divide and multiply numbers that the user inputs from prompt.<br/><br/>
+When you press a button an action is taken, the user inputs the first number, then the operator, then the second number and finally you press equal to calculate your inputs and it displays on the calculator screen.
--- script.js
+++ script.js
@@ -1,19 +1,70 @@
-function operate () {
-    let n1 = parseFloat(prompt("Enter the first number:"));
-    let operator = prompt("Enter an operator (+, -, *, /):");
-    let n2 = parseFloat(prompt("Enter the second number:"));
+
+let operation = {
+        numb1 : null,
+        numb2 : null,
+        operator : null,
+    }
+function calculate (x){
+    
+    if(operation[0] == null || operation[1] == null || operation[2] 
+        == null){
+        if(x == '*' || x == '-' || x == '+' || x == '/' &&
+            operation.operator == null) {
+            alert("Operator is " + operation[2]);    
+            operation[2] = x;
+            alert("new Operator is " + operation[2]); 
+            document.getElementById("display").value = 
+            operation[0] + ' ' + operation[2] + ' ' + operation[1];
+        }
+        if(typeof x === 'number' && operation[0] == null){
+            i = 0; 
+            alert("first number is " + x);
+            operation[i] = x;
+            document.getElementById("display").value = operation[i];
+            i++;
+        } else if (typeof x === 'number' && operation[0] != null &&
+            operation[1] == null){
+            alert("second number is " + x);
+            operation[i] = x;
+            document.getElementById("display").value =
+            operation[0] + ' ' + operation[2] + ' ' + operation[1];
+        }
+    } else if (x == '=' && operation[0] != null && operation[1] != null
+        && operation[2] != null){
+        operate(operation[0], operation[1], operation[2]);
+        
+    }
+    
+
+}
+
+/*function calculate (input) {
+    let number1;
+    let number2;
+    let operator = '';
+    if(number1 === '' && number2 === ''){
+        number1 = input;
+    } else if (number1 !== '' && number2 === ''){
+        number2 = input;
+    } else if (number1 !== '' && number2 !== '' && operator === ''){
+        operator = input;
+    } 
+
+}*/
+
+function operate (n1, n2 , operator) {
     switch (operator) {
         case '+':
             result = n1 + n2;
-            document.getElementById("display").innerHTML = result;
+            document.getElementById("display").value = result;
             break;
         case '-':
             result = n1 - n2;
-            document.getElementById("display").innerHTML = result;
+            document.getElementById("display").value = result;
             break;
         case '*':
             result = n1 * n2;
-            document.getElementById("display").innerHTML = result;
+            document.getElementById("display").value = result;
             break;
         case '/':
             if (n2 === 0) {
@@ -21,11 +72,40 @@ function operate () {
                 return null;
             }
             result = n1 / n2;
-            document.getElementById("display").innerHTML = result;
+            document.getElementById("display").value = result;
             break;
         default:
             return null;
     }
-};
+}
 
-document.getElementById("display").innerHTML = result;
+function cleared (x){
+    if(x == 'AC'){
+    alert("entered Clear all function");
+    document.getElementById("display").value = null;
+    operation[0] = null;
+    operation[1] = null;
+    operation[2] = null;
+    }
+    else if (x == 'C'){
+    alert("entered Clear function");
+    operator[1] = null;
+        
+
+    }
+}
+
+document.addEventListener("keydown", function (event){
+    if(event.key == '*' || event.key == '-' || event.key
+         == '+' || event.key == '/' && operation[2] == null){
+            alert(operation[2]);
+            alert("key press");
+            calculate(event.key);
+         }
+    else if(event.key >= 0 && event.key < 10){
+        calculate(parseInt(event.key));
+    }
+    else if (event.key == '='){
+        calculate(event.key);
+    }
+})
